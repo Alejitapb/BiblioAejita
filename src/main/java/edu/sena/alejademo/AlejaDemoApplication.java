d206f75c6d343e564f6b921f5087eb2a622cf93e
Alejandra Pabón Barbosa <alejandra_pabonb@soy.sena.edu.co>
2025-05-28 10:43:18 -0500

    Ultimos ajustes

--- src/main/java/edu/sena/alejademo/AlejaDemoApplication.java
+++ src/main/java/edu/sena/alejademo/AlejaDemoApplication.java
@@ -2,6 +2,8 @@ package edu.sena.alejademo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class AlejaDemoApplication {
@@ -9,5 +11,4 @@ public class AlejaDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AlejaDemoApplication.class, args);
 	}
-
 }
--- src/main/java/edu/sena/alejademo/MainController.java
+++ src/main/java/edu/sena/alejademo/MainController.java
@@ -1,9 +1,13 @@
-package edu.sena.alejademo.controller;
+package edu.sena.alejademo;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/")
+@SpringBootApplication(scanBasePackages = "edu.sena")
 public class MainController {
 
     @GetMapping("/")
