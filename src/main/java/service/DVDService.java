d35f5cfa68cc00dcbab42aaf81412963e640b345
Alejandra Pabón Barbosa <alejandra_pabonb@soy.sena.edu.co>
2025-05-28 08:44:10 -0500

    Agregando las clases java de service

---
+++ src/main/java/service/DVDService.java
@@ -0,0 +1,43 @@
+package service;
+
+import edu.sena.alejademo.model.DVD;
+import edu.sena.alejademo.repository.DVDRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+public class DVDService {
+
+    @Service
+    public class DVDService {
+
+        @Autowired
+        private DVDRepository dvdRepository;
+
+        public List<DVD> findAll() {
+            return dvdRepository.findAll();
+        }
+
+        public DVD findById(Long id) {
+            return dvdRepository.findById(id)
+                    .orElseThrow(() -> new RuntimeException("DVD no encontrado con id: " + id));
+        }
+
+        public DVD save(DVD dvd) {
+            return dvdRepository.save(dvd);
+        }
+
+        public void deleteById(Long id) {
+            dvdRepository.deleteById(id);
+        }
+
+        public List<DVD> findByGenero(String genero) {
+            return dvdRepository.findByGeneroContaining(genero);
+        }
+
+        public List<DVD> findByDirector(String director) {
+            return dvdRepository.findByDirectorContaining(director);
+        }
+    }
+}
